2f913d144687d349db780e645fc29eb56ea0ef6c
shubhansu-kr <shubhansu2021@gmail.com>
2022-01-10 15:47:50 +0530

    Added a adaptive bubble sort function

--- 26_BubbleSort.cpp
+++ 26_BubbleSort.cpp
@@ -34,6 +34,27 @@ void bubblesort(int A[], int n)
     }
 }
 
+void bubblesortadaptive(int A[], int n)
+{
+    int issorted = 0 ;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        issorted = 1 ;
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (A[j] > A[j+1])
+            {
+                swap(A[j+1], A[j]);
+                issorted = 0 ;
+            }
+        }
+        if (issorted) {
+            break ;
+        }
+    }
+}
+
 int main()
 {
     int n;
