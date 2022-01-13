bbafdefb2e88c160831c9d170ae5deaf18bc8724
shubhansu-kr <shubhansu2021@gmail.com>
2022-01-14 01:37:43 +0530

    Merge sort

--- 30_MergeSort.cpp
+++ 30_MergeSort.cpp
@@ -20,22 +20,110 @@ void display(int A[], int n)
     }
     cout << endl;
 }
+/*
 
-void merge(int A[], int B[], int m, int n)
+int *merge(int A[], int m, int B[], int n)
 {
-    
+    int *C = (int *)malloc(sizeof(int) * (m + n));
+    int i = 0, j = 0, k = 0;
+    while (i < m && j < n)
+    {
+        if (A[i] < B[j])
+        {
+            C[k] = A[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            C[k] = B[j];
+            k++;
+            j++;
+        }
+    }
+    while (i < m)
+    {
+        C[k] = A[i];
+        k++;
+        i++;
+    }
+    while (j < n)
+    {
+        C[k] = B[j];
+        k++;
+        j++;
+    }
+    return C;
+}
+*/
+
+void mergesort(int A[], int low, int mid, int high)
+{
+    int i = low, j = mid + 1, k = 0;
+    int B[high + 1];
+    while (i <= mid && j <= high)
+    {
+        if (A[i] < A[j])
+        {
+            B[k] = A[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            B[k] = A[j];
+            k++;
+            j++;
+        }
+    }
+    while (i <= mid)
+    {
+        B[k] = A[i];
+        k++;
+        i++;
+    }
+    while (j <= high)
+    {
+        B[k] = A[j];
+        k++;
+        j++;
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        A[i] = B[i];
+    }
 }
 
 int main()
 {
+    /* int m;
+    cout << "Enter the size of array A " << endl;
+    cin >> m;
 
-    int n;
-    cout << "Enter the size of array " << endl;
-    cin >> n;
+    int A[m];
+    setarr(A, m);
+    display(A, m); */
 
-    int A[n];
-    setarr(A, n);
-    display(A, n);
+    // int n;
+    // cout << "Enter the size of array B" << endl;
+    // cin >> n;
+
+    // int B[n];
+    // setarr(B, n);
+    // display(B, n);
+
+    /* int *C = merge(A, m, B, n);
+    display(C, n + m); */
+
+    /* int C[m + n];
+    mergesort(A, m, B, n, C);
+    cout << "Merged A and B" << endl;
+    display(C, m + n); */
+
+    int A[] = {2, 23, 43, 3, 33, 65};
+    display(A, 6);
+    mergesort(A, 0, 2, 5);
+    display(A, 6);
 
     return 0;
 }
