cf82f74db1b10f2fc87386fe6f8f18c8a94fce26
shubhansu-kr <shubhansu2021@gmail.com>
2022-01-16 13:05:30 +0530

    BST - check function

---
+++ 37_BST_check.cpp
@@ -0,0 +1,71 @@
+// Check if a Tree is BST or not
+
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createnode(int a)
+{
+    struct node *n = (struct node *)malloc(sizeof(struct node));
+    n->data = a;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+int isBST(struct node *ptr)
+{
+    static struct node *prev = NULL;
+    if (ptr != NULL)
+    {
+        if (isBST(ptr->left) == 0)
+        {
+            return 0;
+        }
+        if (prev != NULL && ptr->data <= prev->data)
+        {
+            return 0;
+        }
+        prev = ptr;
+        return isBST(ptr->right);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int main()
+{
+    struct node *root = createnode(9);
+    struct node *n1 = createnode(4);
+    struct node *n2 = createnode(11);
+    struct node *n3 = createnode(2);
+    struct node *n4 = createnode(7);
+    struct node *n5 = createnode(5);
+    struct node *n6 = createnode(8);
+    struct node *n7 = createnode(19);
+    struct node *n8 = createnode(14);
+
+    root->left = n1;
+    root->right = n2;
+
+    n1->left = n3;
+    n1->right = n4;
+
+    n4->left = n5;
+    n4->right = n6;
+
+    n2->right = n7;
+    n7->left = n8;
+
+    cout << " BST test - " << isBST(root) << endl;
+
+    return 0;
+}
